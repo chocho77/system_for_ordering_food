9f0aff5ca63e7847265c431bd7add9b88d21d303
chocho77 <chavmom@gmail.com>
2025-01-06 00:14:49 +0200

    modified:   __pycache__/database.cpython-311.pyc 	modified:   database.py

--- database.py
+++ database.py
@@ -7,6 +7,7 @@ def insert_data_from_db(key:str,user_item_choice:str,
                 user_item_choice_price:float, 
                 number_of_item:int) -> None:
     user_item_order = (user_item_choice, user_item_choice_price, number_of_item)
+    global d, list_user_item_orders
     list_user_item_orders.append(user_item_order)
     d[key] = list_user_item_orders
 
